59b4d9cb0917cf1ffc47e35e99ada8bc316d0c40
Demi Hayashi <demi.h@me.com>
2022-10-04 19:05:26 -0700

    set up user-controller.js & /user routes

---
+++ controllers/user-controller.js
@@ -0,0 +1,22 @@
+const { User } = require('../models');
+
+const userController = {
+    // GET all users
+
+
+    // GET a single user by _id & populated thought & friend data
+
+
+    // POST a new user
+
+
+    // PUT to update a user by its _id
+
+
+    // DELETE to remove user by its _id
+
+
+    // **BONUS: Remove a user's associated thoughts when deleted**
+};
+
+module.exports = userController;
